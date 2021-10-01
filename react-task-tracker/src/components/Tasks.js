6a06b49fcc68174361b6f1534ccd6483996a06b7
Sibahi <sibahi@live.com>
2021-10-02 00:28:43 +0400

    import tasks.js in app.js

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,30 +1,32 @@
 const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'October 1st at 5 pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'October 2nd at 7 pm',
-        reminder: true,
-    }, 
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'October 3st at 8 pm',
-        reminder: false,
-    }
-]
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "October 1st at 5 pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "October 2nd at 7 pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "October 3st at 8 pm",
+    reminder: false,
+  },
+];
 
 const Tasks = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3>{task.text}</h3>
+      ))}
+    </>
+  );
+};
 
-export default Tasks
+export default Tasks;
