b53475321b6241ad13e9a641616bc072cf6cffdb
Sibahi <sibahi@live.com>
2021-10-01 23:23:03 +0400

    styling with react

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,31 @@
-const Header = (props) => {
-    return (
-        <header>
-          <h1>{props.title}</h1>  
-        </header>
-    )
-}
+import PropTypes from 'prop-types'
 
-export default Header
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <button className='btn'>Add</button>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+/** PropTypes are a mechanism to ensure that components use the correct data type
+ *  and pass the right data, and that components use the right type of props, 
+ * and that receiving components receive the right type of props.
+
+We can think of it like a puppy being delivered to a pet store. 
+The pet store doesn’t want pigs, lions, frogs, or geckos — it wants puppies. 
+PropTypes ensure that the correct data type (puppy) is delivered to 
+the pet store, and not some other kind of animal. */
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+// const headingStyle = {
+//  color: 'red',
+//  backgroundColor: 'black'
+// }
+export default Header;
