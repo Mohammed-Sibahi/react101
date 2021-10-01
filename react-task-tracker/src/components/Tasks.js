0df431621386e98c21c395f8bbdeb869cbe6064f
Sibahi <sibahi@live.com>
2021-10-02 00:38:11 +0400

    add square brackets to the array, debug

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,29 +1,30 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "October 1st at 5 pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "October 2nd at 7 pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "October 3st at 8 pm",
-    reminder: false,
-  },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "October 1st at 5 pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "October 2nd at 7 pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "October 3st at 8 pm",
+      reminder: false,
+    },
+  ]);
   return (
     <>
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   );
