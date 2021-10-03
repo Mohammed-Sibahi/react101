f5c27237f102c01286932a75597db35d78b03695
Sibahi <sibahi@live.com>
2021-10-04 01:15:55 +0400

    shift setTasks function to App.js to make it global

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  // the function setTasks is to update the state
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "October 1st at 5 pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "October 2nd at 7 pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "October 3st at 8 pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,26 +1,7 @@
-import { useState } from "react";
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "October 1st at 5 pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "October 2nd at 7 pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "October 3st at 8 pm",
-      reminder: false,
-    },
-  ]);
+
+const Tasks = ({ tasks }) => {
+  
   return (
     <>
       {tasks.map((task) => (
