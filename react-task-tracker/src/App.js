70f18bbacfc8d8316ae73f46b1cf2ba1cabe1609
Sibahi <sibahi@live.com>
2021-10-05 01:45:31 +0400

    add task function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,6 +4,9 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState 
+  (false)
+
   // the function setTasks is to update the state
   const [tasks, setTasks] = useState([
     {
@@ -25,6 +28,17 @@ function App() {
       reminder: false,
     },
   ]);
+  
+  // Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = {id, ...task  }
+
+  setTasks([...tasks, newTask])
+}
+
+
 
   // delete task
   const deleteTask = (id) => {
@@ -42,8 +56,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- react-task-tracker/src/components/AddTask.js
+++ react-task-tracker/src/components/AddTask.js
@@ -2,12 +2,28 @@ import { useState } from "react";
 
 import React from "react";
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
   const [text, setText] = useState("");
   const [day, setDay] = useState("");
   const [reminder, setReminder] = useState("false");
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // it prevents the submit function to submit to a page
+
+    if (!text) {
+      alert ('Please add a task')
+      return
+    }
+
+    onAdd({ text, day, reminder })
+
+    setText('')
+    setDay('')
+    setReminder(false)
+
+  };
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Task</label>
         <input
@@ -20,14 +36,22 @@ const AddTask = () => {
 
       <div className="form-control">
         <label>Day & Time </label>
-        <input type="text" placeholder="Add Day & Time" value={day}
-          onChange={(e) => setDay(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
 
       <div className="form-control form-control-check">
         <label>Set Reminder</label>
-        <input type="checkbox" value={reminder}
-          onChange={(e) => setReminder(e.currentTarget.checked)} />
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
       <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
