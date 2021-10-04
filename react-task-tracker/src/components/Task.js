71dce8f9abb4cbf1250b1e5661b1d92faa5bfcd8
Sibahi <sibahi@live.com>
2021-10-04 23:51:43 +0400

    create the toggle function

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,13 +1,16 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div
+      className={`task ${task.reminder ? "reminder" : " "}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h3>
         {task.text}{" "}
         <FaTimes
           style={{ color: "red", cursor: "pointer" }}
-          onClick={ () => onDelete(task.id)} // the delete function is executed on click
+          onClick={() => onDelete(task.id)} // the delete function is executed on click
         />
       </h3>
       <p>{task.day}</p>
