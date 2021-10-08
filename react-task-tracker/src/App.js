09ad4947651a74da8d16309f3b8bff1ebee29b6f
Sibahi <sibahi@live.com>
2021-10-08 23:38:09 +0400

    change text and color buttons of add function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -56,7 +56,7 @@ const addTask = (task) => {
 
   return (
     <div className="container">
-      <Header onAdd = {() => setShowAddTask(!showAddTask)} />
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
